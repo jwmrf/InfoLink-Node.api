445fa12de646033f2322b7499169fd223d216830
wilson <jwmrf@cin.ufpe.br>
2019-06-06 23:18:49 -0300

    Separando Métodos + Tratando Falhas

--- app/modules/api.js
+++ app/modules/api.js
@@ -6,56 +6,74 @@ var url = require('url');
 var http = require('https');
 
 let maior = 0;
-let urlFinal = "";
+global.urlFinal = "";
 
 function VideoCrowler() {
 }
 
-VideoCrowler.prototype.start = async function (req, res)  {
+VideoCrowler.prototype.start = async function (req, res) {
 
- 
-        //Transformar em função
-         request('http://www.adorocinema.com/series/serie-7157/video-19562036/', async function (erro, resposta, body) {
-         if(erro){
-            console.log(erro);
-         }   
 
-            var site = jQuery.load(body);
-
-            site('img').each(async function () {
-                var imagem = site(this);
-
-                var tamanho = await PegaTamanho(imagem.attr('src'));
-
-                if (tamanho != undefined) {
-                    if(tamanho.width > tamanho.height && tamanho.type != 'gif'){
-                        if(tamanho.width * tamanho.height > maior){
-                            maior = tamanho.width * tamanho.height;
-                            urlFinal = imagem.attr('src');
-                            console.log("- - - - - - - ");
-                            console.log(tamanho);
-                            //console.log(maior);
-                            console.log(urlFinal);
-                        }
-                    }
-                }
-            })
-        });
-
+  //Transformar em função
+  var retorno = await PegaImagens('https://www.xvideos.com/video48181975/daughter_gets_caught_making_a_video_by_her_dad_kinkycouple111');
+  console.log(retorno);
 };
-  function PegaTamanho (urlImagem){
-    var options =  url.parse(urlImagem); 
-    return new Promise(function(resolve,reject){
-       http.get(urlImagem,function (response){
-            var chunks = [];
-            response.on('data', function (chunk) {
-              chunks.push(chunk);
-            }).on('end',  function() {
-              var buffer =  Buffer.concat(chunks);
-              resolve(sizeOf(buffer));
-            });
-        });
-    })
+function TrataUrl(url) {
+  return new Promise(function (resolve, reject) {
+    let verificaHttp = url.search("http");
+    resolve(verificaHttp);
+  });
+
+
+}
+function PegaImagens(url) {
+  return new Promise(function (resolve, reject) {
+
+    request(url, function (erro, resposta, body) {
+      if (erro) {
+        console.log(erro);
+      }
+
+      var site = jQuery.load(body);
+
+      site('img').each(async function () {
+        var imagem = site(this);
+        let verifica = await TrataUrl(imagem.attr('src'));
+        if (verifica >= 0) {
+          var tamanho = await PegaTamanho(imagem.attr('src'));
+
+          if (tamanho != undefined) {
+            if (tamanho.width > tamanho.height && tamanho.type != 'gif') {
+              if (tamanho.width * tamanho.height > maior) {
+                maior = tamanho.width * tamanho.height;
+                urlFinal = imagem.attr('src');
+                console.log("- - - - - - - ");
+                resolve(urlFinal);
+
+                
+
+              }
+            }
+          }
+        }
+      });
+    });
+  });
+}
+function PegaTamanho(urlImagem) {
+  var options = url.parse(urlImagem);
+  return new Promise(function (resolve, reject) {
+    http.get(urlImagem, function (response) {
+      var chunks = [];
+      response.on('data', function (chunk) {
+        chunks.push(chunk);
+      }).on('end', function () {
+        var buffer = Buffer.concat(chunks);
+        resolve(sizeOf(buffer));
+      });
+
+    });
+  })
 };
 
 module.exports = VideoCrowler;
