bdc98a2da7ec14a3458447c9c59df27c3aef546e
wilson <jwmrf@cin.ufpe.br>
2019-06-09 13:53:01 -0300

    Teste Heroku #2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const variaveis = require('../infoLink.api/config/sistema/variaveis.js');
+const variaveis = require('../app/config/sistema/variaveis.js');
 var teste2 = require('request');
 const bodyParser = require('body-parser');
 
 
 
-servidor = require('../infoLink.api/config/sistema/express');
+servidor = require('../app/config/sistema/express');
 
 servidor.listen(5000,() => { console.log(variaveis.servidor.mensagem)});
 
