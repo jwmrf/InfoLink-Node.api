efb7b5cbec8d7271c4102520b41f89098b5afdb4
wilson <jwmrf@cin.ufpe.br>
2019-06-12 00:33:22 -0300

    Etimeout Resolvido + Compatibildade

--- app/modules/api.js
+++ app/modules/api.js
@@ -12,6 +12,7 @@ function VideoCrowler() {
 }
 
 VideoCrowler.prototype.start = async function (req, res) {
+  urlFinal = "";
   maior = 0;
   //console.log("Entrou na start");
 
@@ -21,50 +22,65 @@ VideoCrowler.prototype.start = async function (req, res) {
 function TrataUrl(url) {
   //console.log("Entrou na trata url");
   return new Promise(function (resolve, reject) {
+    if (url == undefined) {
+      resolve(-1);
+    }
     let verificaHttp = url.search("http");
     resolve(verificaHttp);
   });
 
 
 }
-function PegaImagens(url) {
-  //console.log("Entrou na pega imagens");
-  return new Promise( function (resolve, reject) {
 
-    request(url, function (erro, resposta, body) {
-      if (erro) {
-        resolve("Erro, tente outra url");
-        console.log(erro);
-      }else{
+function Requesta(site) {
+  return new Promise(function (resolve, reject) {
 
-      
+    site('img').each(async function () {
+      var imagem = site(this);
+      let verifica = await TrataUrl(imagem.attr('src'));
+      if (verifica >= 0) {
+        var tamanho = await PegaTamanho(imagem.attr('src'));
 
-      var site = jQuery.load(body);
+        if (tamanho != undefined) {
+          if (tamanho.width > tamanho.height && tamanho.type != 'gif') {
+            if (tamanho.width * tamanho.height > maior) {
+              maior = tamanho.width * tamanho.height;
+              urlFinal = imagem.attr('src');
+              console.log(imagem.attr('src'));
 
-      site('img').each(async function () {
-        var imagem = site(this);
-        let verifica = await TrataUrl(imagem.attr('src'));
-        if (verifica >= 0) {
-          var tamanho = await PegaTamanho(imagem.attr('src'));
 
-          if (tamanho != undefined) {
-            if (tamanho.width > tamanho.height && tamanho.type != 'gif') {
-              if (tamanho.width * tamanho.height > maior) {
-                maior = tamanho.width * tamanho.height;
-                urlFinal = imagem.attr('src');
-                console.log("- - - - - - - ");
-                
-              }
             }
           }
         }
-      });
-    }
-      
+      }
+      if (imagem.attr('src') == site('img').last().attr('src') ) {
+        setTimeout(function () {
+          resolve(urlFinal);
+        }, 5000);
+
+      }
+
     });
-    setTimeout(function(){
-      resolve(urlFinal);
-    },3000);
+  });
+}
+
+function PegaImagens(url) {
+  //console.log("Entrou na pega imagens");
+  return new Promise(function (resolve, reject) {
+
+    request({ method: 'GET', url: url, headers: { 'User-Agent': 'curl/7.47.0' } },async function (erro, resposta, body) {
+      if (erro) {
+        resolve("Erro, tente outra url");
+        console.log(erro);
+      } else {
+        var site = jQuery.load(body);
+        await Requesta(site);
+        resolve(urlFinal);
+
+      }
+
+    });
+
 
   });
 }
