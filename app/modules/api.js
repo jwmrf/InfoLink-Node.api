dacccb5f87044eccb604e5e4f02b49fa6d63f684
Wilson <jwmrf@cin.ufpe.br>
2019-09-26 15:49:23 -0300

    Correções + Melhorias no Resolve
    Correção do bug do schema ( off por enquanto)
    Redução do setTimeout de 1,5 para 1s ( com sobra de tempo, poderia ser 500 ms )
    Correção de erro em que o resolve final era acionado mais de uma vez.

--- app/modules/api.js
+++ app/modules/api.js
@@ -10,9 +10,9 @@ mongoose.connect('mongodb://admin:arkantos2008@ds255107.mlab.com:55107/info-link
 
 class VideoCrowler {
   constructor() {
-    this._id =  ""
+    this._id = ""
   }
-  
+
   //Método inicial que recebe a url do website.
   async start(req, res) {
 
@@ -37,12 +37,18 @@ class VideoCrowler {
   Requesta(site) {
     return new Promise(function (resolve, reject) {
       let lista = [];
+      let listaVertical = [];
       let maior = 0;
+      let numero = site('img').length;
       if (undefined == site('img').last().attr('src')) {
-        lista.unshift("Erro, tente outra url");
+        listaVertical, lista.unshift("Erro, tente outra url");
       }
-      site('img').each(async function () {
+      if (numero == 0) {
+        resolve(lista[0])
+      }
 
+
+      site('img').each(async function (index, value) {
         var imagem = site(this);
         let verifica = await new VideoCrowler().TrataUrl(imagem.attr('src'));
         if (verifica >= 0) {
@@ -53,36 +59,48 @@ class VideoCrowler {
                 maior = tamanho.width * tamanho.height;
                 lista.unshift(imagem.attr('src'));
               }
+            } else if (tamanho.width < tamanho.height && tamanho.type != 'gif') {
+              listaVertical.unshift(imagem.attr('src'));
             }
           }
         }
         // Quando a imagem do 'Each' for a última da página, ele dispara o timer de 2 segundos para retorno
-        if (imagem.attr('src') == site('img').last().attr('src')) {
-          setTimeout( async function () {
-            linkSchema("teste",this._id,lista[0]);
-            resolve(lista[0]);
-          }, 1500);
+        if (imagem.attr('src') == site('img').last().attr('src') && numero == index + 1) {
+          setTimeout(async function () {
+            //linkSchema("teste", this._id, lista[0]);
+            if (lista[0] != "Erro, tente outra url") {
+              resolve(lista[0]);
+            } else {
+              resolve(listaVertical[0]);
+            }
+          }, 1000);
         }
       });
     });
   }
 
   PegaImagens(url) {
-    return new Promise(function (resolve, reject) {
-      request({ method: 'GET', url: url, headers: { 'User-Agent': 'curl/7.47.0' } }, async function (erro, resposta, body) {
-        if (erro) {
-          resolve("Erro, tente outra url");
-          console.log(erro);
-        } else {
-          var site = jQuery.load(body);
-          var maiorImagem = await new VideoCrowler().Requesta(site);
-          resolve(maiorImagem);
-        }
-      });
+    return new Promise(async function (resolve, reject) {
+      let verifica = await new VideoCrowler().TrataUrl(url);
+      if (verifica >= 0) {
+
+        request({ method: 'GET', url: url, headers: { 'User-Agent': 'curl/7.47.0' } }, async function (erro, resposta, body) {
+          if (erro) {
+            resolve("Erro, tente outra url");
+            console.log(erro);
+          } else {
+            var site = jQuery.load(body);
+            var maiorImagem = await new VideoCrowler().Requesta(site);
+            resolve(maiorImagem);
+          }
+        });
+      } else {
+        resolve("Erro, tente outra url");
+      }
     });
   }
 
-  //Utilizia biblioteca Image-size, dando get no endereço da imagem e retornando suas propriedades.
+  //Utilizi a biblioteca Image-size, dando get no endereço da imagem e retornando suas propriedades.
   PegaTamanho(urlImagem) {
     var options = url.parse(urlImagem);
     return new Promise(function (resolve, reject) {
--- app/schemas/linksSchema.js
+++ app/schemas/linksSchema.js
@@ -3,30 +3,31 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
-async function  Inserir(url, update = "", imagem = ""){
-    console.log("AUI ENTROU")
-    if(update == ""){
-        const linkModel = new schema({
-           url: {trim: true, index: true, required: true, type: String} ,
-           imagem: {required: false, type: String} ,
-           dataCriacao: {type: Date,default: Date.now}
-        },{ versionKey: false});
-        mongoose.model('teste3', linkModel);
-        const data = mongoose.model('teste3')
-        var id = await new data ({
-             url: url,
-             dataCriacao: new Date(),
-           }).save().then((res) => {
-               console.log(" INSERIU POW <3 <3 ")
-               return res._id
-           }).catch((error) => {
-             return ""
-             console.log(" pow vei n deu ")
-           })
-           if(id != ""){
-               return id
-           }
-    }else{
+async function Inserir(url, update = "", imagem = "") {
+    console.log("")
+    if (update == "") {
+        /*
+const linkModel = new schema({
+  url: {trim: true, index: true, required: true, type: String} ,
+  imagem: {required: false, type: String} ,
+  dataCriacao: {type: Date,default: Date.now}
+},{ versionKey: false});
+mongoose.model('teste3', linkModel);
+const data = mongoose.model('teste3')
+var id = await new data ({
+    url: url,
+    dataCriacao: new Date(),
+  }).save().then((res) => {
+      console.log(" INSERIU POW <3 <3 ")
+      return res._id
+  }).catch((error) => {
+    return ""
+    console.log(" pow vei n deu ")
+  })
+  if(id != ""){
+      return id
+  } */
+    } else {
         //const data = mongoose.model('teste3')
         //console.log(data.find())
         // data.findByIdAndUpdate(update,{$set: {
