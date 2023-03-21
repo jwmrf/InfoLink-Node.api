e4101e62130a2711e759edd36ccbd27d2c69eb95
wilson <jwmrf@cin.ufpe.br>
2023-03-21 03:01:17 -0300

    vercels

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Express on Vercels");
 });
 
-app.listen(6000, () => {
-  console.log("Running on port 6000.");
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
 
 // Export the Express API
