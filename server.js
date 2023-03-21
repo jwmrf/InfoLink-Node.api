5d19cffafbe9c274b78ef8bc8198c57818885691
wilson <jwmrf@cin.ufpe.br>
2023-03-21 06:08:25 -0300

    Atualização Layoyt Consulta + server

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-//const express = require("express");
+const variaveis = require('./config/sistema/variaveis');
+var teste2 = require('request');
 const bodyParser = require('body-parser');
-const servidor = require('./config/sistema/express');
-const app = express();
+
+
+
 servidor = require('./config/sistema/express');
+
+servidor.listen(process.env.PORT || 5000,() => { console.log(variaveis.servidor.mensagem)});
 
 servidor.use(bodyParser.json());
 servidor.use(bodyParser.urlencoded({extended: false}));
-//app.get("/", (req, res) => {
-  //res.send("Express on Vercels");
-//});
-
-//app.listen(5000, () => {
-  //console.log("Running on port 5000.");
-//});
-
-// Export the Express API
-module.exports = servidor;
